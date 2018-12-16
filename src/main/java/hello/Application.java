3f76f137f1ba84e74652b99b4dae95f9d424ce76
esenu <esenu@itu.edu.tr>
2018-12-16 06:00:22 +0200

    fifth attempt in github
    I can register new user to db
    I can show all users by get request of "authentication"
    I can return "world" with get request of "hello"
    I created login page but I couldn't complete all authentication kind of staffes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,8 @@
 package hello;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -14,13 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("hello")
 public class Application {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
-
-
-
-
 }
